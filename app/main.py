082225a3bf7df6867aa82e9e6cf783113b7d686e
Mark A. Miller <MAM@lbl.gov>
2023-01-09 16:17:01 -0500

    removed ayncs

--- app/main.py
+++ app/main.py
@@ -2,15 +2,14 @@
 https://github.com/turbomam/fastapi_docker
 """
 
+# todo mypy --strict app
+
 import csv
 import json
-
-# import urllib.parse
+from typing import Dict, List, Union, Tuple, Any
 
 import requests
 
-# import aiofiles
-# from typing import Union
 from deepdiff import DeepDiff  # type: ignore
 
 # import linkml
@@ -24,8 +23,8 @@ from linkml_runtime import SchemaView  # type: ignore
 from linkml_runtime.dumpers import json_dumper  # type: ignore
 from pydantic import BaseModel, Field, AnyUrl
 
-names_ages_file = "names_ages.tsv"
-names_ages_data = {}
+# names_ages_file = "names_ages.tsv"
+# names_ages_data: List[Dict[str, Union[str, int]]] = []
 
 app = FastAPI()
 
@@ -33,25 +32,30 @@ app = FastAPI()
 schema_url = "https://raw.githubusercontent.com/microbiomedata/nmdc-schema/main/src/schema/nmdc.yaml"
 schema_view = SchemaView(schema_url)
 
-# just showing how to return TSV
-# Open the TSV file in read mode
-with open(names_ages_file, "r") as tsv_file:
-    # Create a DictReader object to read the TSV file
-    reader = csv.DictReader(tsv_file, delimiter="\t")
 
-    # Iterate over the rows in the TSV file
-    for row in reader:
-        # overwrites ages if there are duplicate names
-        names_ages_data[row["Name"]] = row["Age"]
+# # just showing how to return TSV
+# # Open the TSV file in read mode
+# with open(names_ages_file, "r") as tsv_file:
+#     # Create a DictReader object to read the TSV file
+#     reader = csv.DictReader(tsv_file, delimiter="\t")
+#
+#     # Iterate over the rows in the TSV file
+#     for row in reader:
+#         # overwrites ages if there are duplicate names
+#         names_ages_data[row["Name"]] = row["Age"]
+
+# todo: Elais encourages async, but SO answer XXX discourages in situation XXX
 
 
 @app.get("/")
-def read_root():
-    return {"greetings": "NMDC users!"}
+# async
+def read_root() -> str:
+    return "see /docs for API documentation"
 
 
 @app.get("/get_global_slot/{slot_name}")
-def get_global_slot(slot_name: str):
+# async
+def get_global_slot(slot_name: str) -> Any:
     slot_obj = schema_view.get_slot(slot_name)
     slot_json = json_dumper.dumps(slot_obj)
     # fastapi custom json serializer?
@@ -61,7 +65,8 @@ def get_global_slot(slot_name: str):
 
 
 @app.get("/get_slot_class_usage/{slot_name}/{class_name}")
-def get_slot_class_usage(slot_name: str, class_name: str):
+# async
+def get_slot_class_usage(slot_name: str, class_name: str) -> Any:
     usage_slot_obj = schema_view.induced_slot(slot_name, class_name)
     usage_slot_json = json_dumper.dumps(usage_slot_obj)
     usage_slot_dict = json.loads(usage_slot_json)
@@ -71,7 +76,8 @@ def get_slot_class_usage(slot_name: str, class_name: str):
 # @app.get("/slot_class_usage/{global_schema_url}/{slot_name}/{class_name}/{usage_schema_url}")
 @app.get("/get_global_usage_diff/{slot_name}/{class_name}")
 # def get_global_usage_diff(global_schema_url: str, slot_name: str, class_name: str, usage_view: str):
-def get_global_usage_diff(slot_name: str, class_name: str):
+# async
+def get_global_usage_diff(slot_name: str, class_name: str) -> Any:
     """
     Provide the name of a slot in the NMDC schema, and the name of a class that uses that slot.
     A DeepDiff difference between the slot's global definition and it's usage within the class will be returned.
@@ -93,24 +99,27 @@ def get_global_usage_diff(slot_name: str, class_name: str):
     return global_vs_usage
 
 
-@app.get("/names_ages_tsv")
-async def names_ages_tsv():
-    # would be better to stream the file
-    return FileResponse(names_ages_file, media_type="text/tab-separated-values")
-    # return FileResponse(path=names_ages_file, filename=names_ages_file)
+# @app.get("/names_ages_tsv")
+# # async
+# def names_ages_tsv():
+# # would be better to stream the file
+#     return FileResponse(names_ages_file, media_type="text/tab-separated-values")
+#     # return FileResponse(path=names_ages_file, filename=names_ages_file)
 
 
-@app.get("/static_oreos")
-async def static_oreos():
-    response = requests.get(
-        "https://world.openfoodfacts.org/api/v0/product/7622300489434.json"
-    )
-    return response.json()
+# @app.get("/static_oreos")
+# # async
+# def static_oreos():
+# response = requests.get(
+#         "https://world.openfoodfacts.org/api/v0/product/7622300489434.json"
+#     )
+#     return response.json()
 
 
 # @app.get("/get_from_dub_encoded/{double_encoded_url}")
-# async def get_from_dub_encoded(double_encoded_url: str):
-#     """
+# # async
+# def get_from_dub_encoded(double_encoded_url: str):
+# """
 #     Try https%253A%252F%252Fworld.openfoodfacts.org%252Fapi%252Fv0%252Fproduct%252F7622300489434.json
 #     """
 #     original_url = urllib.parse.unquote(urllib.parse.unquote(double_encoded_url))
@@ -124,32 +133,28 @@ class InputModel(BaseModel):
     )
 
 
-@app.post("/unencoded_url")
-def unencoded_url(inputs: InputModel):
-    response = requests.get(inputs.unencoded_url)
-    return response.json()
+# @app.post("/unencoded_url")
+# def unencoded_url(inputs: InputModel):
+#     response = requests.get(inputs.unencoded_url)
+#     return response.json()
 
 
-@app.post("/unencoded_form")
-def unencoded_form(
-    url: str = Form(
-        default="https://world.openfoodfacts.org/api/v0/product/7622300489434.json"
-    ),
-):
-    """
-    Try https://world.openfoodfacts.org/api/v0/product/7622300489434.json
-    """
-    response = requests.get(url)
-    return response.json()
-
-
-# @app.post("/login/")
-# async def login(username: str = Form(...), password: str = Form(...)):
-#     return {"username": username, "password": password}
+# @app.post("/unencoded_form")
+# def unencoded_form(
+#     url: str = Form(
+#         default="https://world.openfoodfacts.org/api/v0/product/7622300489434.json"
+#     ),
+# ):
+#     """
+#     Try https://world.openfoodfacts.org/api/v0/product/7622300489434.json
+#     """
+#     response = requests.get(url)
+#     return response.json()
 
 
 @app.get("/get_class_typecode/{class_name}")
-def get_class_typecode(class_name: str):
+# async
+def get_class_typecode(class_name: str) -> Union[tuple[str, int], Any]:
     # Validate input
     # todo this doesn't actually return a 400 error
     if not isinstance(class_name, str) or not class_name:
@@ -209,6 +214,7 @@ def get_class_typecode(class_name: str):
 
 
 @app.post("/undefined_mixs_assigned_terms/")
+# async
 def undefined_mixs_assigned_terms(
     def_file_url: str = Form(
         default="https://raw.githubusercontent.com/GenomicsStandardsConsortium/mixs/issue-511-tested-schemasheets/schemasheets/tsv_in/MIxS_6_term_updates_global_partial_slotdefs.tsv"
@@ -216,7 +222,7 @@ def undefined_mixs_assigned_terms(
     assignment_file_url: str = Form(
         default="https://raw.githubusercontent.com/GenomicsStandardsConsortium/mixs/issue-511-tested-schemasheets/schemasheets/tsv_in/MIxS_6_term_updates_slot_assignments_and_usages.tsv"
     ),
-):
+) -> List[str]:
     """
     Return a list of all terms in the def_file that are not assigned in the assignment_file
     """
@@ -244,8 +250,11 @@ def undefined_mixs_assigned_terms(
 
     return undefined_terms
 
+    # return ["a", "b", "c"]
+
 
 @app.post("/unassigned_mixs_defined_terms/")
+# async ?
 def unassigned_mixs_defined_terms(
     def_file_url: str = Form(
         default="https://raw.githubusercontent.com/GenomicsStandardsConsortium/mixs/issue-511-tested-schemasheets/schemasheets/tsv_in/MIxS_6_term_updates_global_partial_slotdefs.tsv"
@@ -253,7 +262,7 @@ def unassigned_mixs_defined_terms(
     assignment_file_url: str = Form(
         default="https://raw.githubusercontent.com/GenomicsStandardsConsortium/mixs/issue-511-tested-schemasheets/schemasheets/tsv_in/MIxS_6_term_updates_slot_assignments_and_usages.tsv"
     ),
-):
+) -> List[str]:
     """
     Return a list of all terms in the def_file that are not assigned in the assignment_file
     """
@@ -282,9 +291,13 @@ def unassigned_mixs_defined_terms(
     return unassigned_terms
 
 
-def tsv_url_to_term_list(tsv_url, term_column_name, discard_first_n=2):
+# async
+def tsv_url_to_term_list(
+    tsv_url: str, term_column_name: str, discard_first_n: int = 2
+) -> List[str]:
     # Download the TSV file from the URL
     response = requests.get(tsv_url)
+    # print(f"response type: {type(response).class_name}")
 
     # Decode the contents of the response
     text = response.text
@@ -301,4 +314,49 @@ def tsv_url_to_term_list(tsv_url, term_column_name, discard_first_n=2):
 
     remaining_items.sort()
 
+    remaining_items = remaining_items
+
     return remaining_items
+
+
+@app.post("/compare_slots_in_two_classes/")
+# async?
+def compare_slots_in_two_classes(
+    schema_1_url: str = Form(
+        default="https://raw.githubusercontent.com/GenomicsStandardsConsortium/mixs/main/model/schema/mixs.yaml"
+    ),
+    class_1_name: str = Form(default="water"),
+    schema_2_url: str = Form(
+        default="https://raw.githubusercontent.com/microbiomedata/sheets_and_friends/main/artifacts/nmdc_submission_schema.yaml"
+    ),
+    class_2_name: str = Form(default="water"),
+) -> Dict[str, List[str]]:
+    # which source for mixs schema?
+    """
+    SLOW! Should cache returned views, at least. Compare the slots in two schemas and return the set differences
+    """
+    # todo refactor
+    view_1 = SchemaView(schema_1_url)
+    class_1_slots_obj_list = view_1.class_induced_slots(class_1_name)
+    class_1_slots_names = [slot_obj["name"] for slot_obj in class_1_slots_obj_list]
+    class_1_slots_names = list(set(class_1_slots_names))
+    class_1_slots_names.sort()
+    schema_1_name = view_1.schema.name
+
+    view_2 = SchemaView(schema_2_url)
+    class_2_slots_obj_list = view_2.class_induced_slots(class_2_name)
+    class_2_slots_names = [slot_obj["name"] for slot_obj in class_2_slots_obj_list]
+    class_2_slots_names = list(set(class_2_slots_names))
+    class_2_slots_names.sort()
+    schema_2_name = view_2.schema.name
+
+    class_1_only = list(set(class_1_slots_names) - set(class_2_slots_names))
+    class_1_only.sort()
+
+    class_2_only = list(set(class_2_slots_names) - set(class_1_slots_names))
+    class_2_only.sort()
+
+    return {
+        f"Slots only found in {schema_1_name}'s {class_1_name}": class_1_only,
+        f"Slots only found in {schema_2_name}'s {class_2_name}": class_2_only,
+    }
